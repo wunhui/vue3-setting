efe3889ecc3b73f77ac558b01d701e79565aea6f
황웅희 <dndgml99@freeive.com>
2023-07-21 16:50:16 +0900

    first commit

---
+++ src/components/H-header.vue
@@ -0,0 +1,10 @@
+<template>
+    <header class="l-header">
+        header 입니다.
+    </header>
+</template>
+
+<script setup></script>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="l-wrap">
+      <header class="l-header">
+
+      </header>
+      <div
+        class="l-container"
+      >
+        <aside class="l-sidebar">
+        </aside>
+
+      </div>
+    </div>
+  </template>
+  <script setup>
+  
+  </script>
+  
+  <style lang="scss" scoped>
+  </style>
+  
